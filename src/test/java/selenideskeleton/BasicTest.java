2400825afb44fee2be537d6513ca16c136ae57ae
Egor <l.e.andr@inbox.ru>
2018-04-30 01:37:56 +0500

    Mail processing example

--- src/test/java/selenideskeleton/BasicTest.java
+++ src/test/java/selenideskeleton/BasicTest.java
@@ -1,44 +1,57 @@
 package selenideskeleton;
 
+import org.bouncycastle.util.encoders.Base64;
 import org.testng.Assert;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
-import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.testng.annotations.Report;
 
-import utils.Mail;
-
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
-import static com.codeborne.selenide.WebDriverRunner.url;
 
 @Report
-public class BasicTest extends TestBase {
+public class BasicTest {
+	
+	@BeforeSuite()
+	public void suiteSetup() {
+		Configuration.browser = "phantomjs";
+		System.setProperty("phantomjs.binary.path", "./ext/phantomjs.exe");
+		Configuration.timeout = 10000;
+	}
 	
 	@Test()
 	public void exampleTest() {
-		GoogleSearchPage mainPage = open("https://www.google.ru", GoogleSearchPage.class);
-		mainPage.waitPageLoaded();
-		mainPage.searchFiled.setValue("Цетральный банк РФ");
-		mainPage.searchFiled.pressEnter();
-		GoogleResultsPage resultsPage = Selenide.page(GoogleResultsPage.class);
-		resultsPage.waitPageLoaded();
-		String cbrUrl = "https://www.cbr.ru/";
-		resultsPage.clickOnLink("Центральный банк Российской Федерации", cbrUrl);
-		CBRMainPage cbrMain = Selenide.page(CBRMainPage.class);
-		cbrMain.waitPageLoaded();
-		Assert.assertEquals(url(), cbrUrl);
-		cbrMain.receptionLink.click();
-		CBRReceptionPage reception = Selenide.page(CBRReceptionPage.class);
-		reception.waitPageLoaded();
-		reception.getContentButtonByText("Написать благодарность").click();
-		CBRMessageRegisterPage messagePage = Selenide.page(CBRMessageRegisterPage.class);
-		messagePage.waitPageLoaded();
-		messagePage.messageArea.sendKeys("текст благодарности");
-		messagePage.agreementCheckbox.setSelected(true);
-		String screenshotPath = "./target/surefire-reports/exampleTest";
-		screenshot(screenshotPath);
-		Mail.send("targetAddress", "fromAddress", "password", "smtp_host", "Message title", 
-				System.getProperty("user.dir") + "\\build\\reports\\tests\\target\\surefire-reports\\exampleTest.png");
+		String login = "executort@bk.ru";
+		String pwd = "MTExTGV0dGVycw";
+		String subject = "Узнайте о супервозможностях Почты Mail.Ru";
+		String keyword = "супервозможностях";
+		String fromName = "Команда Mail.ru";
+		String fromEmail = "welcome@corp.mail.ru";
+		String bodyPart = "Спасибо, что зарегистрировались в Почте Mail.Ru";
+		
+		MailBasePage basePage = open("https://mail.ru", MailBasePage.class);
+		basePage.waitPageLoaded();
+		MailboxPage mailbox = basePage.login(login, new String(Base64.decode((pwd + "==").getBytes())));
+		mailbox.waitPageLoadedForUser(login);
+		MailboxItem target = null;
+		for (MailboxItem curItem : mailbox.search(keyword)) {
+			if (curItem.subject.text().startsWith(subject)) {
+				target = curItem;
+				break;
+			}
+		}
+		Assert.assertTrue(target != null, "Can't find target mail in search results");
+		Mail mail = target.open();
+		mail.waitMailLoaded();
+		mail.subject.shouldHave(exactText(subject));
+		Assert.assertEquals(fromName, mail.getSender().getName());
+		Assert.assertEquals(fromEmail, mail.getSender().getEmail());
+		mail.body.shouldHave(text(bodyPart));
+		mailbox.logout.click();
+		basePage.waitPageLoaded();
 	}
 
 }
